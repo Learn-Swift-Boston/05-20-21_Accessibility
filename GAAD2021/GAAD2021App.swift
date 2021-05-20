d0d016f9ea896b87815b36624fd9bc2952969ec1
Matt Dias <matt.dias6@gmail.com>
2021-05-20 16:41:21 -0400

    adding empty project

---
+++ GAAD2021/GAAD2021App.swift
@@ -0,0 +1,17 @@
+//
+//  GAAD2021App.swift
+//  GAAD2021
+//
+//  Created by Matt Dias on 5/20/21.
+//
+
+import SwiftUI
+
+@main
+struct GAAD2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
