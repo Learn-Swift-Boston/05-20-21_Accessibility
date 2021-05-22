b2c453e529f19733eed04451b08929e3b30c6bc8
Matt Dias <matt.dias6@gmail.com>
2021-05-22 19:01:42 -0400

    Adding the code from the session

--- GAAD2021/ContentView.swift
+++ GAAD2021/ContentView.swift
@@ -8,16 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isTrue = false
+    @State var size: Double = 0.0
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State var progress = 0.0
     var body: some View {
 
         NavigationView {
             VStack(alignment: .leading) {
+                Button("Hey Chat!") {
+
+                }
+                ProgressView("Progress", value: progress, total: 10)
+                Toggle("I love this app!", isOn: $isTrue)
+                Slider(value: $size, in: 0...10) {
+                    Text("size")
+                }
                 Text("Pick Your Favorite Emoji").bold()
-                EmojiPicker()
+                EmojiPicker()//.frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
 
             }.padding(8)
             .navigationTitle("Emoji Picker")
         }
+//        .onReceive(timer) { _ in
+//            if progress < 10 {
+//                progress += 1
+//            } else {
+//                progress = 0
+//                UIAccessibility.post(notification: .announcement, argument: "download finished!")
+//            }
+//        }
     }
 }
 
--- GAAD2021/EmojiCell.swift
+++ GAAD2021/EmojiCell.swift
@@ -14,6 +14,10 @@ struct EmojiCell: View {
             Text("\(codepointString)")
                 .font(.footnote)
         }
+        .accessibilityElement(children: .ignore)
+        .accessibility(label: Text("\(emoji) emoji, code point: \(codepointString).                                     "))
+        .accessibility(hint: Text("double tap to select"))
+        .accessibility(addTraits: isSelected ? [.isSelected] : [])
         .padding(4)
         .background(isSelected ? Color.accentColor : Color.clear)
         .onTapGesture {
